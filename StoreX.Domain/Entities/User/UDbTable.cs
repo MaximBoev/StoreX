2d92b4d18aa1281a04179f0422d501466c2ed53c
maxim boev <maxim-boev@mail.ru>
2024-03-26 11:10:00 +0200

    base data test1

---
+++ StoreX.Domain/Entities/User/UDbTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreX.Domain.Entities.User
+{
+    public class UDbTable
+    {
+        
+    }
+}
